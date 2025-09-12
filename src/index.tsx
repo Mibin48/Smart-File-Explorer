e56601d8f524ca59e37f8ccab6852d10764e13de
Mibin Benny <mibinbenny7@gmail.com>
2025-09-12 22:19:28 +0530

    Removed useless files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import TestApp from './TestApp';
 import '../styles.css';
 
 const container = document.getElementById('root');
